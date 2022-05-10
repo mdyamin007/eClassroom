54dde70bc6eaf86653a7616dc2230649241dc53a
Md Yamin <mdyamin007@yahoo.com>
2022-05-10 18:09:01 +0600

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,27 @@ import Slide from "./Slide";
 import Student from "./Student";
 import Teacher from "./Teacher";
 import PageNotFound from "./PageNotFound";
+import "../styles/index.css";
 
 function App() {
   return (
     <>
+      {/* <div className="mt-4 d-flex justify-content-center align-items-center">
+        <div className="glass__container">
+          <div className="p-4 text-center">
+            <h1>Please donate money!</h1>
+            <p>
+              To keep up and running the website again please donate some money.
+            </p>
+            <p>
+              We need money to renew the domain and for some additional cost.
+            </p>
+            <h5 className="fw-bold">Bkash: 01915395544</h5>
+            <h5 className="fw-bold">Nagad: 01915395544</h5>
+            <h5 className="fw-bold">Rocket: 019153955448</h5>
+          </div>
+        </div>
+      </div> */}
       <AuthProvider>
         <Router>
           <Switch>
